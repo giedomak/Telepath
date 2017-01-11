e7355507023a675e72440110aa36e17d1e1e2dc7
Giedo Mak <giedomak@gmail.com>
2017-01-11 16:08:52 +0100

    mvn javadoc:javadoc will no be inside the repo

--- src/main/java/pathdb/kpathindex/KPathIndex.java
+++ src/main/java/pathdb/kpathindex/KPathIndex.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (C) 2017-2017 - All rights reserved.
+ * This file is part of the pathdb project which is released under the GPLv3 license.
+ * See file LICENSE.txt or go to http://www.gnu.org/licenses/gpl.txt for full license details.
+ * You may use, distribute and modify this code under the terms of the GPLv3 license.
+ */
+
 package pathdb.kpathindex;
 
 import com.pathdb.pathIndex.Path;
@@ -15,6 +22,7 @@ public interface KPathIndex {
    *
    * @param pathPrefix The prefix of a path which we need to search
    * @return An Iterable with Paths which satisfy the pathPrefix
+   * @throws IOException I/O error
    */
   Iterable<Path> search(PathPrefix pathPrefix) throws IOException;
 
