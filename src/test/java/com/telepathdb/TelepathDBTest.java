7f37b7d75520a971872cf90b1fcec5f94b7072a2
Giedo Mak <giedomak@gmail.com>
2017-07-27 01:19:43 +0200

    remove unused imports

--- src/test/java/com/telepathdb/TelepathDBTest.java
+++ src/test/java/com/telepathdb/TelepathDBTest.java
@@ -7,20 +7,11 @@
 
 package com.telepathdb;
 
-import com.telepathdb.datamodels.utilities.Logger;
-import com.telepathdb.memorymanager.MemoryManager;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
-import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.notNull;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
 
 public class TelepathDBTest {
 
