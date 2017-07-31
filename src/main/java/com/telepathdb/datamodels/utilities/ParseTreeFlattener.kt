3e7fa9ae5843a6124a1ddfa52630a80350aa950c
Giedo Mak <giedomak@gmail.com>
2017-08-01 00:05:17 +0200

    add some documentation to the parsetree flattener

--- src/main/java/com/telepathdb/datamodels/utilities/ParseTreeFlattener.kt
+++ src/main/java/com/telepathdb/datamodels/utilities/ParseTreeFlattener.kt
@@ -1,7 +1,7 @@
 /**
  * Copyright (C) 2016-2017 - All rights reserved.
  * This file is part of the telepathdb project which is released under the GPLv3 license.
- * See file LICENSE.txt or go to http://www.gnu.org/licenses/gpl.txt for full license details.
+ * See file LICENSE.txt or go to http: *www.gnu.org/licenses/gpl.txt for full license details.
  * You may use, distribute and modify this code under the terms of the GPLv3 license.
  */
 
@@ -9,19 +9,57 @@ package com.telepathdb.datamodels.utilities
 
 import com.telepathdb.datamodels.ParseTree
 
+/**
+ * Flatten ParseTrees into multi-trees.
+ *
+ * We escalate the children from a node to its parent if both the node and its parent have the same operator.
+ *
+ * Example input:
+ * <pre>
+ *            CONCATENATION
+ *          /       |        \
+ *         a  CONCATENATION   f
+ *              /      \
+ *          UNION       e
+ *         /    \
+ *        b    UNION
+ *             /  \
+ *            c    d
+ * </pre>
+ *
+ * Output:
+ * <pre>
+ *        CONCATENATION
+ *        /   |    |  \
+ *       a  UNION  e   f
+ *          / | \
+ *         b  c  d
+ * </pre>
+ */
 object ParseTreeFlattener {
 
+    /**
+     * Flatten a given ParseTree recursively into a multi-tree.
+     *
+     * @param parseTree The given input ParseTree.
+     * @return A flattened ParseTree from the given input.
+     */
     fun flatten(parseTree: ParseTree): ParseTree {
 
+        // Break recursion once we've found a leaf.
         if (parseTree.isLeaf) return parseTree
 
+        // Since we could be escalating multiple children from the same parent, we have to track these escalations.
         var offset = 0
 
         for (index in parseTree.children.indices) {
 
+            // Flatten each child recursively
             val child = parseTree.children[index + offset]
             val flattenedChild = flatten(child)
 
+            // The subtree rooted at flattenedChild, should all be flattened now.
+            // So escalate its children if the operators match.
             if (child.operatorId == parseTree.operatorId) {
 
                 parseTree.children.removeAt(index + offset)
@@ -31,6 +69,7 @@ object ParseTreeFlattener {
             }
         }
 
+        // We're done
         return parseTree
     }
 }
