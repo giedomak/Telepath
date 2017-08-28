623b85487e7ef4b4222edc1af50ff3b93676ed9f
Giedo Mak <giedomak@gmail.com>
2017-08-28 19:20:21 +0200

    add comments to index-lookup and add specs for it

--- src/main/java/com/github/giedomak/telepathdb/physicaloperators/IndexLookup.kt
+++ src/main/java/com/github/giedomak/telepathdb/physicaloperators/IndexLookup.kt
@@ -4,8 +4,26 @@ import com.github.giedomak.telepathdb.datamodels.graph.PathPrefix
 import com.github.giedomak.telepathdb.datamodels.graph.PathStream
 import com.github.giedomak.telepathdb.datamodels.plans.PhysicalPlan
 
+/**
+ * Physical operator to lookup paths in the kPathIndex.
+ *
+ * Let's say we've got this physical plan:
+ *
+ *      INDEX_LOOKUP
+ *        /  |  \
+ *       a   b   c
+ *
+ * Then `a - b - c` forms the labeled path for which we are searching for paths in de index.
+ *
+ * @property physicalPlan The physical plan which holds the leafs which make up the pathId.
+ */
 class IndexLookup(override val physicalPlan: PhysicalPlan) : PhysicalOperator {
 
+    /**
+     * Evaluate the index lookup and stream the results.
+     *
+     * @return PathStream with the results of the index lookup.
+     */
     override fun evaluate(): PathStream {
         return PathStream(
                 physicalPlan.query.telepathDB.kPathIndex.search(
@@ -16,8 +34,10 @@ class IndexLookup(override val physicalPlan: PhysicalPlan) : PhysicalOperator {
         )
     }
 
+    /**
+     * Cost of an index lookup is very cheap.
+     */
     override fun cost(): Long {
         return physicalPlan.cardinality()
     }
-
 }
---
+++ src/test/java/com/github/giedomak/telepathdb/physicaloperators/IndexLookupTest.kt
@@ -0,0 +1,52 @@
+package com.github.giedomak.telepathdb.physicaloperators
+
+import com.github.giedomak.telepathdb.TelepathDB
+import com.github.giedomak.telepathdb.datamodels.PathTest
+import com.github.giedomak.telepathdb.datamodels.Query
+import com.github.giedomak.telepathdb.datamodels.graph.Edge
+import com.github.giedomak.telepathdb.datamodels.graph.Path
+import com.github.giedomak.telepathdb.datamodels.graph.PathPrefix
+import com.github.giedomak.telepathdb.datamodels.plans.PhysicalPlanTest
+import com.github.giedomak.telepathdb.datamodels.stores.PathIdentifierStore
+import com.github.giedomak.telepathdb.kpathindex.KPathIndexInMemory
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
+import org.junit.Test
+import kotlin.streams.toList
+import kotlin.test.assertEquals
+
+class IndexLookupTest {
+
+    @Test
+    fun evaluates() {
+
+        val pathId = 42L
+
+        val paths = listOf(
+                Path(pathId, PathTest.increasingNodes(3, 42)),
+                Path(pathId, PathTest.increasingNodes(3, 49))
+        )
+
+        val store = mock<PathIdentifierStore> {
+            on { getPathIdByEdges(listOf(Edge("a"), Edge("b"))) }.doReturn(pathId)
+        }
+
+        val index = mock<KPathIndexInMemory> {
+            on { search(PathPrefix(42)) }.doReturn(paths.stream())
+        }
+
+        val telepathdb = mock<TelepathDB> {
+            on { kPathIndex }.doReturn(index)
+            on { pathIdentifierStore }.doReturn(store)
+        }
+
+        val query = mock<Query> {
+            on { telepathDB }.doReturn(telepathdb)
+        }
+
+        val physicalPlan = PhysicalPlanTest.generatePhysicalPlan(PhysicalOperator.INDEX_LOOKUP, listOf("a", "b"), query)
+
+        assertEquals(paths, physicalPlan.physicalOperator!!.evaluate().paths.toList())
+    }
+}
