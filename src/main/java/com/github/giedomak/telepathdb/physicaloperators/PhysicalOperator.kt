692027b0cb3c8a5f2dc3496d1e9a4fbd05087217
Giedo Mak <giedomak@gmail.com>
2017-08-29 14:42:35 +0200

    add docs to the physical operator interface

--- src/main/java/com/github/giedomak/telepathdb/physicaloperators/PhysicalOperator.kt
+++ src/main/java/com/github/giedomak/telepathdb/physicaloperators/PhysicalOperator.kt
@@ -3,6 +3,13 @@ package com.github.giedomak.telepathdb.physicaloperators
 import com.github.giedomak.telepathdb.datamodels.graph.PathStream
 import com.github.giedomak.telepathdb.datamodels.plans.PhysicalPlan
 
+/**
+ * Interface for physical operators.
+ *
+ * @property physicalPlan The physical plan holds information regarding the sets on which to operate on.
+ * @property firstChild The first set of data to operate on.
+ * @property lastChild The last set of data to operate on.
+ */
 interface PhysicalOperator {
 
     val physicalPlan: PhysicalPlan
@@ -10,10 +17,23 @@ interface PhysicalOperator {
     val firstChild get() = physicalPlan.children.first().physicalOperator!!
     val lastChild get() = physicalPlan.children.last().physicalOperator!!
 
+    /**
+     * Evaluates the physical operator and produces a PathStream.
+     *
+     * @return PathStream with the results of the evaluation.
+     */
     fun evaluate(): PathStream
 
+    /**
+     * Calculates the cost of the physical operation.
+     *
+     * @return The cost of the physical operation.
+     */
     fun cost(): Long
 
+    /**
+     * Delegate cardinality to the physical plan.
+     */
     fun cardinality(): Long {
         return physicalPlan.cardinality()
     }
@@ -37,6 +57,12 @@ interface PhysicalOperator {
 
         // ------ FUNCTIONS -------
 
+        /**
+         * Convert the operators constants to an actual [PhysicalOperator] instance.
+         *
+         * @param physicalPlan which holds the operator constant.
+         * @return The PhysicalOperator instance which has knowledge of the physical plan.
+         */
         fun getPhysicalOperator(physicalPlan: PhysicalPlan): PhysicalOperator? {
 
             return when (physicalPlan.operator) {
