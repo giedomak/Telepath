f9173032b9aac26c466fd6f1e45f9b5e2bae8bae
Giedo Mak <giedomak@gmail.com>
2017-09-05 20:01:30 +0200

    add specs for synopsis cardinality estimation with sliding window

--- src/test/java/com/github/giedomak/telepathdb/cardinalityestimation/SynopsisCardinalityEstimationTest.kt
+++ src/test/java/com/github/giedomak/telepathdb/cardinalityestimation/SynopsisCardinalityEstimationTest.kt
@@ -41,4 +41,35 @@ class SynopsisCardinalityEstimationTest {
         }
     }
 
+    @Test
+    fun returnsTheCardinalityOfAdvancedJoins() {
+
+        // | T r/l1/l2 | = | T r/l1 | * ( l1/l2.two / l1.in )
+        val synopsis = mock<Synopsis> {
+            on { pairs(Pair(Edge("a"), Edge("b"))) }.doReturn(400)
+            on { two(Pair(Edge("b"), Edge("c"))) }.doReturn(1)
+            on { `in`(Edge("b"))}.doReturn(10)
+        }
+
+        val cardinalityEstimation = spy(SynopsisCardinalityEstimation(mock()))
+        cardinalityEstimation.synopsis = synopsis
+
+        //             HASH_JOIN
+        //             /       \
+        //     INDEX_LOOKUP  INDEX_LOOKUP
+        //          |            /  \
+        //          a           b    c
+        val child1 = PhysicalPlanTest.generatePhysicalPlan(PhysicalOperator.INDEX_LOOKUP, listOf("a"))
+        val child2 = PhysicalPlanTest.generatePhysicalPlan(PhysicalOperator.INDEX_LOOKUP, listOf("b", "c"))
+
+        PhysicalOperator.JOIN_OPERATORS.forEach {
+
+            val physicalPlan = PhysicalPlanTest.generatePhysicalPlanWithChildren(it, listOf(child1, child2))
+
+            val actual = cardinalityEstimation.getCardinality(physicalPlan)
+
+            assertEquals(40L, actual)
+        }
+    }
+
 }
